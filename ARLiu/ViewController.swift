82b3f25208f9fb30e9edf35b1d894e67af1b26dd
Mac Mini Novo <macmininew@Mac-mini-de-Mac.local>
2019-04-11 17:39:56 -0300

    Add placing objects, display and crashes it

---
+++ ARLiu/ViewController.swift
@@ -0,0 +1,216 @@
+//
+//  ViewController.swift
+//  ARLiu
+//
+//  Created by Mac Mini Novo on 11/04/19.
+//  Copyright © 2019 LiuVasconcelos. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    @IBOutlet weak var timerlabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        //set the physics delegate
+        sceneView.scene.physicsWorld.contactDelegate = self
+        
+        self.addTargetNodes()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    @IBAction func clickOnAxeButton(_ sender: Any) {
+        self.fireMissile(type: "axe")
+    }
+    
+    @IBAction func clickOnBananaButton(_ sender: Any) {
+        self.fireMissile(type: "banana")
+    }
+    
+    func getUserVector() -> (SCNVector3, SCNVector3) { 
+        if let frame = self.sceneView.session.currentFrame {
+            let mat = SCNMatrix4(frame.camera.transform)
+            let dir = SCNVector3(-1 * mat.m31, -1 * mat.m32, -1 * mat.m33)
+            let pos = SCNVector3(mat.m41, mat.m42, mat.m43)
+            
+            return (dir, pos)
+        }
+        return (SCNVector3(0, 0, -1), SCNVector3(0, 0, -0.2))
+    }
+    
+    func createMissile(type : String)->SCNNode{
+        var node = SCNNode()
+        
+        switch type {
+        case "banana":
+            let scene = SCNScene(named: "art.scnassets/banana.dae")
+            node = (scene?.rootNode.childNode(withName: "Cube_001", recursively: true)!)!
+            node.scale = SCNVector3(0.2,0.2,0.2)
+            node.name = "banana"
+        case "axe":
+            let scene = SCNScene(named: "art.scnassets/axe.dae")
+            node = (scene?.rootNode.childNode(withName: "axe", recursively: true)!)!
+            node.scale = SCNVector3(0.3,0.3,0.3)
+            node.name = "bathtub"
+        default:
+            node = SCNNode()
+        }
+        
+        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        node.physicsBody?.isAffectedByGravity = false
+        
+        node.physicsBody?.categoryBitMask = CollisionCategory.missileCategory.rawValue
+        node.physicsBody?.collisionBitMask = CollisionCategory.targetCategory.rawValue
+        return node
+    }
+    
+    func fireMissile(type : String){
+        var node = SCNNode()
+        node = createMissile(type: type)
+        
+        let (direction, position) = self.getUserVector()
+        node.position = position
+        var nodeDirection = SCNVector3()
+        switch type {
+        case "banana":
+            nodeDirection  = SCNVector3(direction.x*4,direction.y*4,direction.z*4)
+            node.physicsBody?.applyForce(nodeDirection, at: SCNVector3(0.1,0,0), asImpulse: true)
+        case "axe":
+            nodeDirection  = SCNVector3(direction.x*4,direction.y*4,direction.z*4)
+            node.physicsBody?.applyForce(SCNVector3(direction.x,direction.y,direction.z), at: SCNVector3(0,0,0.1), asImpulse: true)
+        default:
+            nodeDirection = direction
+        }
+        
+        node.physicsBody?.applyForce(nodeDirection , asImpulse: true)
+        
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    func addTargetNodes(){
+        for index in 1...100 {
+            
+            var node = SCNNode()
+            
+            if (index > 9) && (index % 10 == 0) {
+                let scene = SCNScene(named: "art.scnassets/mouthshark.dae")
+                node = (scene?.rootNode.childNode(withName: "shark", recursively: true)!)!
+                node.scale = SCNVector3(0.3,0.3,0.3)
+                node.name = "shark"
+            }else{
+                let scene = SCNScene(named: "art.scnassets/bath.dae")
+                node = (scene?.rootNode.childNode(withName: "Cube_001", recursively: true)!)!
+                node.scale = SCNVector3(0.02,0.02,0.02)
+                node.name = "bath"
+            }
+            
+            node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+            node.physicsBody?.isAffectedByGravity = false
+            
+            node.position = SCNVector3(randomFloat(min: -10, max: 10),randomFloat(min: -4, max: 5),randomFloat(min: -10, max: 10))
+            
+            let action : SCNAction = SCNAction.rotate(by: .pi, around: SCNVector3(0, 1, 0), duration: 1.0)
+            let forever = SCNAction.repeatForever(action)
+            node.runAction(forever)
+            
+            node.physicsBody?.categoryBitMask = CollisionCategory.targetCategory.rawValue
+            node.physicsBody?.contactTestBitMask = CollisionCategory.missileCategory.rawValue
+            
+            sceneView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
+    func randomFloat(min: Float, max: Float) -> Float {
+        return (Float(arc4random()) / 0xFFFFFFFF) * (max - min) + min
+    }
+    
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        
+        print("** Collision!! " + contact.nodeA.name! + " hit " + contact.nodeB.name!)
+        
+        if contact.nodeA.physicsBody?.categoryBitMask == CollisionCategory.targetCategory.rawValue
+            || contact.nodeB.physicsBody?.categoryBitMask == CollisionCategory.targetCategory.rawValue {
+            
+            if (contact.nodeA.name! == "shark" || contact.nodeB.name! == "shark") {
+                score+=5
+            }else{
+                score+=1
+            }
+            
+            DispatchQueue.main.async {
+                contact.nodeA.removeFromParentNode()
+                contact.nodeB.removeFromParentNode()
+                self.scoreLabel.text = String(self.score)
+            }
+            
+        }
+    }
+}
+
+
+struct CollisionCategory: OptionSet {
+    let rawValue: Int
+    
+    static let missileCategory  = CollisionCategory(rawValue: 1 << 0)
+    static let targetCategory = CollisionCategory(rawValue: 1 << 1)
+    static let otherCategory = CollisionCategory(rawValue: 1 << 2)
+}
