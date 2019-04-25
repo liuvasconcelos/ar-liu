44aca8304f9d037dd5179950bdf413a3d7cbd169
liuvasconcelos <ltenoriovasconcelos@gmail.com>
2019-04-24 22:27:13 -0300

    develop Add songs, explosion effects and deleted not used models.
    - Also add score and finish game.
    
    Signed-off-by: liuvasconcelos <ltenoriovasconcelos@gmail.com>

--- ARLiu/Main/MainViewController.swift
+++ ARLiu/Main/MainViewController.swift
@@ -18,6 +18,10 @@ class MainViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContact
     @IBOutlet weak var scoreLabel: UILabel!
     
     var score = 0
+    var player: AVAudioPlayer?
+    var seconds = 10
+    var timer = Timer()
+    var isTimerRunning = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +36,8 @@ class MainViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContact
         sceneView.scene.physicsWorld.contactDelegate = self
         
         self.addTargetNodes()
-        
+        self.playBackgroundMusic()
+        self.runTimer()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -102,8 +107,8 @@ class MainViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContact
         
         switch type {
         case "banana":
-            let scene = SCNScene(named: "art.scnassets/banana.dae")
-            node = (scene?.rootNode.childNode(withName: "Cube_001", recursively: true)!)!
+            let scene = SCNScene(named: "art.scnassets/Orange.dae")
+            node = (scene?.rootNode.childNode(withName: "Orange", recursively: true)!)!
             node.scale = SCNVector3(0.15,0.15,0.15)
             node.name = "banana"
         case "axe":
@@ -134,9 +139,11 @@ class MainViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContact
         case "banana":
             nodeDirection  = SCNVector3(direction.x*4,direction.y*4,direction.z*4)
             node.physicsBody?.applyForce(nodeDirection, at: SCNVector3(0.1,0,0), asImpulse: true)
+            playSound(sound: "noise", format: "mp3")
         case "axe":
             nodeDirection  = SCNVector3(direction.x*4,direction.y*4,direction.z*4)
             node.physicsBody?.applyForce(SCNVector3(direction.x,direction.y,direction.z), at: SCNVector3(0,0,0.1), asImpulse: true)
+            playSound(sound: "bomb", format: "mp3")
         default:
             nodeDirection = direction
         }
@@ -203,7 +210,71 @@ class MainViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContact
                 contact.nodeB.removeFromParentNode()
                 self.scoreLabel.text = String(self.score)
             }
+            playSound(sound: "explosion", format: "wav")
             
+            let  explosion = SCNParticleSystem(named: "Explode", inDirectory: nil)
+            contact.nodeB.addParticleSystem(explosion!)
+        }
+    }
+    
+    func playSound(sound: String, format: String) {
+        guard let url = Bundle.main.url(forResource: sound, withExtension: format) else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func playBackgroundMusic(){
+        let audioNode = SCNNode()
+        let audioSource = SCNAudioSource(fileNamed: "babyshark.mp3")!
+        let audioPlayer = SCNAudioPlayer(source: audioSource)
+        
+        audioNode.addAudioPlayer(audioPlayer)
+        
+        let play = SCNAction.playAudio(audioSource, waitForCompletion: true)
+        audioNode.runAction(play)
+        sceneView.scene.rootNode.addChildNode(audioNode)
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(self.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if seconds == 0 {
+            timer.invalidate()
+            gameOver()
+        }else{
+            seconds -= 1
+            timerlabel.text = "\(seconds)"
+        }
+        
+    }
+    
+    func resetTimer(){
+        timer.invalidate()
+        seconds = 10
+        timerlabel.text = "\(seconds)"
+    }
+    
+    func gameOver(){
+        if let score = scoreLabel.text {
+            let alertController = UIAlertController(title: "", message: "Acabou jogo! Seu score foi: \(score)", preferredStyle: .alert)
+            self.present(alertController, animated: true, completion: nil)
+            
+            let time = DispatchTime.now() + 3
+            DispatchQueue.main.asyncAfter(deadline: time) {
+                alertController.dismiss(animated: true, completion: nil)
+                self.navigationController?.popViewController(animated: true)
+            }
         }
     }
 }
